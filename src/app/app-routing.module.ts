ea5363e60df45207ad498e4d9af26b6c5a2b3434
Alegria Aclan <aaclan@ebi.ac.uk>
2019-07-10 21:23:29 +0100

    Angular workshop app (206 - component events)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,37 @@
+// This example shows the particular variation of routing syntax
+// needed to work with the CLI lazy loading support. The syntax can be
+// slightly different if you are using a different tooling approach.
+
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { ExtraOptions, RouterModule, Routes } from '@angular/router';
 
+// Note there is no import of the HrFilesSearchComponent or reference
+// to './hr-files-search.component'.
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./dashboard/dashboard.module').then(m => m.DashboardModule)
+  },
+];
+
+const config: ExtraOptions = {
+  useHash: true,
+  enableTracing: false, // Turn this on to log routing events to the console
+  scrollPositionRestoration: 'enabled'
+  // ,preloadingStrategy: PreloadAllModules
+};
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, config)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
+
+// Behind the scenes, Angular converts the above syntax to something like
+// this:
+
+// loadChildren: () =>
+// System.import('./hr/hr.module').then(({HrModule}) => HrModule)
---
+++ src/app/dashboard/video-list/video-list.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+
+
+@Component({
+  selector: 'app-video-list',
+  templateUrl: './video-list.component.html',
+  styleUrls: ['./video-list.component.css']
+})
+
+export class VideoListComponent implements OnInit {
+  @Input() videos: Video[];
+  @Output('select') selectVideo : EventEmitter<Video> = new EventEmitter<Video>();
+
+  selectedVideo;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  select(video){
+    this.selectedVideo = video;
+    this.selectVideo.emit(video);
+  }
+}
